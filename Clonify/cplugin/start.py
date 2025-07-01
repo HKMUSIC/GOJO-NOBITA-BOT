146a4b542982f2ef5c1d183c43b5912e2852c654
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-01 16:29:40 +0530

    Update start.py

--- Clonify/cplugin/start.py
+++ Clonify/cplugin/start.py
@@ -49,11 +49,11 @@ async def start_pm(client, message: Message, _):
     
     await loading_1.edit_text("<b>ʟᴏᴀᴅɪɴɢ</b>")
     C_BOT_SUPPORT_CHAT = await get_cloned_support_chat(a.id)
-    C_SUPPORT_CHAT = f"https://t.me/lll_DrX_network_lll"
+    C_SUPPORT_CHAT = f"https://t.me/GOJO_NOBITA_II"
     # await asyncio.sleep(0.1)
     await loading_1.edit_text("<b>ʟᴏᴀᴅɪɴɢ.</b>")
     C_BOT_SUPPORT_CHANNEL = await get_cloned_support_channel(a.id)
-    C_SUPPORT_CHANNEL = f"https://t.me/lll_drx_network_lll"
+    C_SUPPORT_CHANNEL = f"https://t.me/NOBITA_MUSIC_SUPPORT"
     # await asyncio.sleep(0.1)
     await loading_1.edit_text("<b>ʟᴏᴀᴅɪɴɢ..</b>")
     await asyncio.sleep(0.1)
