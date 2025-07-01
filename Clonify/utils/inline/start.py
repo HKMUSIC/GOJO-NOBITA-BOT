ec367d6b1b8959b3c7c6e3024378657b6762570d
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-01 23:46:28 +0530

    Update start.py

--- Clonify/utils/inline/start.py
+++ Clonify/utils/inline/start.py
@@ -33,8 +33,8 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=url=f"https://github.com/HKMUSIC/Fantastic-gc")
+            InlineKeyboardButton(text=[𝗗𝗘𝗩], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=[𝗖𝗢-𝗗𝗘𝗩], url=f"https://t.me/ll_NOBITA_DEFAULTERS_ll")
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
