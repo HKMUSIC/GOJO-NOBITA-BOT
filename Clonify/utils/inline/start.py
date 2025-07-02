0297329e448eeced21faa499fad7b3d84d1aa633
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-02 07:04:47 +0530

    Update start.py

--- Clonify/utils/inline/start.py
+++ Clonify/utils/inline/start.py
@@ -33,8 +33,8 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=["S_B_10"], url=f"https://t.me/ll_NOBITA_DEFAULTERS_ll")
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_10"], url=f"https://t.me/ll_NOBITA_DEFAULTERS_ll")
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
